8a4574daf5bde27bdb89c8730006f991b206aecf
ilyaf f <ilyaf@appveyor.com>
2017-10-05 16:09:37 -0700

    Add project files.

---
+++ multiple-test-assemblies/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace multiple_test_assemblies
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
